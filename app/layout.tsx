3677e00421b77595fb226bca39c9ad21b6ca4e7d
alanmoraales <alanmoraales845@gmail.com>
2022-11-16 15:44:53 -0600

    refactor using arrow syntax

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,18 @@
 import { Open_Sans } from "@next/font/google";
+import { IRFC } from "@declarations/common";
 import "./globals.css";
 
 const openSansFont = Open_Sans({ weight: ["300", "400", "500", "700"] });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" className={openSansFont.className}>
-      {/*
+const RootLayout = ({ children }: IRFC) => (
+  <html lang="en" className={openSansFont.className}>
+    {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  );
-}
+    <head />
+    <body>{children}</body>
+  </html>
+);
+
+export default RootLayout;
