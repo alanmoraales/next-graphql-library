602d4e4d4e9b99256bcd3d9f7970e05223ea0b66
alanmoraales <alanmoraales845@gmail.com>
2022-12-06 13:36:12 -0600

    rendering list of books

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,59 @@
 import BookCard from "@molecules/BookCard/BookCard";
+import GridList from "components/organisms/GridList/GridList";
 
 const Home = () => (
   <div>
     <h1>Available Books</h1>
-    <BookCard
-      title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
-      coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
-    />
+    <GridList>
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+      <BookCard
+        title="Wigetta 1 blah blah blah blah blah blah, Wigetta 1 blah blah blah blah blah blah"
+        coverUrl="https://m.media-amazon.com/images/I/91onbOzEJYL.jpg"
+      />
+    </GridList>
   </div>
 );
 
