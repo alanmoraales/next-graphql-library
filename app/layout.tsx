548aea44ffb388c0cf870fedb83f81511126a720
alanmoraales <alanmoraales845@gmail.com>
2022-12-06 11:19:55 -0600

    remove IRFC interface

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
+import { ReactNode } from "react";
 import { Open_Sans } from "@next/font/google";
-import { IRFC } from "@declarations/common";
 import "./globals.css";
 
 const openSansFont = Open_Sans({ weight: ["300", "400", "500", "700"] });
 
-const RootLayout = ({ children }: IRFC) => (
+interface IRootLayoutProps {
+  children: ReactNode;
+}
+
+const RootLayout = ({ children }: IRootLayoutProps) => (
   <html lang="en" className={openSansFont.className}>
     {/*
         <head /> will contain the components returned by the nearest parent
--- declarations/common/IRFC.ts
+++
@@ -1,7 +0,0 @@
-import { FC, ReactNode } from "react";
-
-interface IRFC extends FC {
-  children: ReactNode;
-}
-
-export type { IRFC };
