560e9651884573d6364e386cbfd17a93b3dca0f4
alanmoraales <alanmoraales845@gmail.com>
2023-01-25 03:28:20 -0600

    simplify using new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
+
+import { useEffect, useState } from "react";
 import { useQuery, gql } from "@apollo/client";
+import { Flex, useDisclosure } from "@chakra-ui/react";
+import ContentContainer from "@atoms/ContentContainer";
 import BookCard from "@molecules/BookCard";
-import GridList from "components/organisms/GridList";
+import GridList from "@organisms/GridList";
+import AppBar from "@organisms/AppBar";
+import BookDetailsDrawer from "@organisms/BookDetailsDrawer";
 
 type Book = {
   title: string;
@@ -29,24 +35,56 @@ const useAllBooks = (): Book[] => {
 
 const Home = () => {
   const books = useAllBooks();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedBook, setSelectedBook] = useState<Book | null>(null);
+
+  useEffect(() => {
+    if (selectedBook) {
+      onOpen();
+    }
+  }, [selectedBook, onOpen]);
+
+  useEffect(() => {
+    if (!isOpen) {
+      setSelectedBook(null);
+    }
+  }, [isOpen]);
+
+  const onReadSynopsis = (book: Book) => () => {
+    setSelectedBook(book);
+  };
 
   return (
-    <div>
-      <h1>Available Books</h1>
-      <GridList>
-        {books.map(({ title, imageUrl, id, availableQuantity }) => (
-          <BookCard
-            key={id}
-            title={title}
-            coverUrl={imageUrl}
-            availableQuantity={availableQuantity}
-            onAddToCart={() => {}}
-            detailsUrl="/"
-            isAddingToCart={false}
-          />
-        ))}
-      </GridList>
-    </div>
+    <Flex direction="column" gap={4}>
+      <AppBar />
+      <ContentContainer paddingY={8} gap={8}>
+        <GridList>
+          {books.map((book) => {
+            const { title, imageUrl, id, availableQuantity } = book;
+            return (
+              <BookCard
+                key={id}
+                title={title}
+                coverUrl={imageUrl}
+                availableQuantity={availableQuantity}
+                onReadSynopsis={onReadSynopsis(book)}
+                onAddToCart={() => {}}
+                isAddingToCart={false}
+              />
+            );
+          })}
+        </GridList>
+        <BookDetailsDrawer
+          isOpen={Boolean(isOpen && selectedBook)}
+          onClose={onClose}
+          onAddToCart={() => {}}
+          isAddingToCart={false}
+          bookTitle={selectedBook?.title || ""}
+          bookDescription={selectedBook?.description || ""}
+          bookImageUrl={selectedBook?.imageUrl || ""}
+        />
+      </ContentContainer>
+    </Flex>
   );
 };
 
