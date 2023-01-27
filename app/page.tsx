20b5d1064132ca9155aaf3146386ff925b4bafac
alanmoraales <alanmoraales845@gmail.com>
2023-01-26 18:03:36 -0600

    use generated code

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,16 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useQuery, gql } from "@apollo/client";
 import { Flex, useDisclosure } from "@chakra-ui/react";
 import ContentContainer from "@atoms/ContentContainer";
 import BookCard from "@molecules/BookCard";
 import GridList from "@organisms/GridList";
 import AppBar from "@organisms/AppBar";
 import BookDetailsDrawer from "@organisms/BookDetailsDrawer";
-
-type Book = {
-  title: string;
-  description: string;
-  id: number;
-  imageUrl: string;
-  availableQuantity: number;
-};
-
-const useAllBooks = (): Book[] => {
-  const query = gql`
-    {
-      allBooks {
-        title
-        description
-        id
-        imageUrl
-        availableQuantity
-      }
-    }
-  `;
-  const queryResponse = useQuery(query);
-  return (queryResponse.data?.allBooks || []) as Book[];
-};
+import { Book, useAllBooksQuery } from "services/graphql";
 
 const Home = () => {
-  const books = useAllBooks();
+  const { data } = useAllBooksQuery();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [selectedBook, setSelectedBook] = useState<Book | null>(null);
 
@@ -59,7 +35,7 @@ const Home = () => {
       <AppBar />
       <ContentContainer paddingY={8} gap={8}>
         <GridList>
-          {books.map((book) => {
+          {data?.allBooks.map((book) => {
             const { title, imageUrl, id, availableQuantity } = book;
             return (
               <BookCard
