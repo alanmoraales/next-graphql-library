8546507241664981d15080769d9530076444941e
alanmoraales <alanmoraales845@gmail.com>
2023-01-29 00:30:25 -0600

    create AppTemplate component

---
+++ components/organisms/AppTemplate/AppTemplate.tsx
@@ -0,0 +1,17 @@
+import { Flex } from "@chakra-ui/react";
+import AppBar from "@organisms/AppBar";
+
+interface IAppTemplateProps {
+  children: React.ReactNode;
+}
+
+const AppTemplate = ({ children }: IAppTemplateProps) => {
+  return (
+    <Flex direction="column" gap={4}>
+      <AppBar />
+      {children}
+    </Flex>
+  );
+};
+
+export default AppTemplate;
