ba2c35cbc342a4ee2f27406981662423d6352796
alanmoraales <alanmoraales845@gmail.com>
2023-01-29 16:16:32 -0600

    move emojis constant to Emoji component

--- components/atoms/Emoji/Emoji.tsx
+++ components/atoms/Emoji/Emoji.tsx
@@ -1,11 +1,18 @@
-import emojis from "constants/emojis";
+const htmlEmojis = {
+  book: "&#128214;",
+  cart: "&#128722;",
+  person: "&#129489;",
+  wavingHand: "&#128075;",
+  callMeHand: "&#129305;",
+  victoryHand: "&#9996;&#65039;",
+};
 
 interface IEmojiProps {
-  name: keyof typeof emojis;
+  name: keyof typeof htmlEmojis;
 }
 
 const Emoji = ({ name }: IEmojiProps) => {
-  return <span dangerouslySetInnerHTML={{ __html: emojis[name] }} />;
+  return <span dangerouslySetInnerHTML={{ __html: htmlEmojis[name] }} />;
 };
 
 export default Emoji;
