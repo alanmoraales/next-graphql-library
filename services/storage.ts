331125b9d6fe7e2742d05026912e5cd1ea2465d6
alanmoraales <alanmoraales845@gmail.com>
2023-02-02 21:43:00 -0600

    avoid using localStorage on server

--- services/storage.ts
+++ services/storage.ts
@@ -1,18 +1,29 @@
 const StorageService = () => {
-  // A function that reads userToken from localStorage
-  const getUserToken = () => {
-    return localStorage?.getItem("userToken") || "";
+  // A higher order function that validates if localStorage is available
+  const withLocalStorageAvailable = (fn: Function) => {
+    if (typeof localStorage === "undefined") {
+      return () => {};
+    }
+    return fn;
   };
+
+  // A function that reads userToken from localStorage
+  const getUserToken = withLocalStorageAvailable(() => {
+    return localStorage?.getItem("userToken") || "";
+  });
 
   // A function that writes userToken to localStorage
-  const setUserToken = (token: string) => {
+  const setUserToken = withLocalStorageAvailable((token: string) => {
+    console.log("setUserToken", token);
     localStorage?.setItem("userToken", token);
-  };
+  });
 
   // A function that removes userToken from localStorage
-  const removeUserToken = () => {
+  const removeUserToken = withLocalStorageAvailable(() => {
+    console.log("removeUserToken");
     localStorage?.removeItem("userToken");
-  };
+    console.log("removeddddUserToken");
+  });
 
   return {
     getUserToken,
