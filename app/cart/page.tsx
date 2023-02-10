1e3f0265c7a1e5a5fedd2c9b251954f70182089b
alanmoraales <alanmoraales845@gmail.com>
2023-02-09 23:20:51 -0600

    refactor using new components

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,24 +1,21 @@
 "use client";
 
-import BookCoverImage from "@atoms/BookCoverImage";
+import { Grid, Heading } from "@chakra-ui/react";
 import ContentContainer from "@atoms/ContentContainer";
-import { Button, Flex, Grid, Heading, Text } from "@chakra-ui/react";
-import { useUserCartQuery } from "services/graphql";
+import {
+  CartSummaryContent,
+  CartSummaryDesktopCard,
+  CartSummaryMobileCard,
+} from "@organisms/CartSummary";
+import CartList from "@organisms/CartList";
 
 const Cart = () => {
-  const { data } = useUserCartQuery();
-  const cartItems = data?.cart.items || [];
-  const cartItemsCount = cartItems.reduce(
-    (acc, item) => acc + item.quantity,
-    0
-  );
-
   return (
     <>
       <ContentContainer
         paddingY={8}
         gap={8}
-        paddingBottom="calc(var(--chakra-space-8) + 129px)"
+        paddingBottom={{ base: "calc(var(--chakra-space-8) + 129px)", md: 8 }}
       >
         <Heading size="lg">Your Cart</Heading>
         <Grid
@@ -31,87 +28,15 @@ const Cart = () => {
           gap={16}
           position="relative"
         >
-          <Flex direction="column" gap={4}>
-            {cartItems.map((item) => (
-              <Grid
-                key={item.id}
-                gridTemplateColumns="30% 1fr"
-                gap={4}
-                borderWidth="1px"
-                borderRadius="md"
-                padding={4}
-              >
-                <BookCoverImage
-                  src={item.book.coverSrc}
-                  alt={item.book.title}
-                  isSquare
-                />
-                <Flex direction="column" gap={2} justifyContent="space-between">
-                  <Flex direction="column" gap={1}>
-                    <Heading size="sm" noOfLines={2}>
-                      {item.book.title}
-                    </Heading>
-                    <Text fontSize="sm">
-                      {item.book.author}, {item.book.year}.
-                    </Text>
-                  </Flex>
-                  <Text fontSize="sm">{item.quantity} added.</Text>
-                  <Button
-                    variant="outline"
-                    colorScheme="red"
-                    size="sm"
-                    width="fit-content"
-                  >
-                    Remove
-                  </Button>
-                </Flex>
-              </Grid>
-            ))}
-          </Flex>
-          <Flex
-            direction="column"
-            gap={4}
-            display={{ base: "none", md: "flex" }}
-            borderWidth="thin"
-            height="fit-content"
-            borderRadius="md"
-            padding={4}
-            position="sticky"
-            top="105px"
-          >
-            <Flex direction="column" gap={2}>
-              <Heading size="md">Summary</Heading>
-              <Text fontSize="md">
-                {`You're selecting ${cartItemsCount} books to pickup later. You'll receive an email with a QR code to pickup your books.`}
-              </Text>
-            </Flex>
-            <Button colorScheme="purple" variant="solid" size="md">
-              Finish reservation
-            </Button>
-          </Flex>
+          <CartList />
+          <CartSummaryDesktopCard>
+            <CartSummaryContent />
+          </CartSummaryDesktopCard>
         </Grid>
       </ContentContainer>
-      <Flex
-        borderTopWidth="thin"
-        position="fixed"
-        bottom="0"
-        width="100%"
-        background="rgba(255, 255, 255, 0.9)"
-        backdropFilter="blur(14.5px)"
-        display={{ base: "flex", md: "none" }}
-      >
-        <ContentContainer gap={4} paddingY={8}>
-          <Flex direction="column" gap={2}>
-            <Heading size="sm">Summary</Heading>
-            <Text fontSize="sm">
-              {`You're selecting ${cartItemsCount} books to pickup later. You'll receive an email with a QR code to pickup your books.`}
-            </Text>
-          </Flex>
-          <Button colorScheme="purple" variant="solid" size="sm">
-            Finish reservation
-          </Button>
-        </ContentContainer>
-      </Flex>
+      <CartSummaryMobileCard>
+        <CartSummaryContent />
+      </CartSummaryMobileCard>
     </>
   );
 };
